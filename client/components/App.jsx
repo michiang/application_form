ffea7d4874df0c556b356c1ddcd4d738fbdb2385
Michael Chiang <michaelchiang@michaels-mbp.attlocal.net>
2017-06-15 12:13:07 -0700

    Import sass for css customization

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,12 @@ export default class App extends Component {
 	constructor() {
 		super();
 
-		this.state = {};
+		this.state = {
+			firstName: ""
+		};
+
+		this.firstNameValidate = this.firstNameValidate.bind(this);
+		this.lastNameValidate = this.lastNameValidate.bind(this);
 
 	}
 	componentDidMount() {
@@ -24,6 +29,7 @@ export default class App extends Component {
   render() {
     return (
     <div>
+     <div className="card-panel teal lighten-2">SimpleForms</div>
      <div style={{textAlign: 'center'}}>
         <h1>Apply for Jersey Mike's</h1>
         <p> Fill out the following form to apply for an open position at Jersey Mikes</p>
@@ -57,7 +63,7 @@ export default class App extends Component {
 
   	</div>
   	<br/> <br/> <br/> 
-  	    <p> 1. Personal Info </p> <hr /> <br />
+  	    <p> 2. Personal Info </p> <hr /> <br />
 
       <div className="row">
         <div className="input-field col s6">
@@ -81,14 +87,82 @@ export default class App extends Component {
           <label htmlFor="confirm">Email</label>
         </div>
       </div>
-      <div className="input-field col s6">
+      <div className="row">
+      	<div className="input-field col s7">
           <input id="icon_telephone" type="tel" className="validate"></input>
           <label htmlFor="icon_telephone">Phone Number</label>
         </div>
-      <div className="input-field col s6" style={{display:'inline'}}>
-          <input id="icon_telephone" type="tel" className="validate"></input>
-          <label htmlF	or="icon_telephone">Birth Date</label>
+      	<div className="input-field col s6">
+          <input id="birthdate" type="tel" className="validate"></input>
+          <label htmlFor="birthdate">Birth Date</label>
         </div>
+      <div className="row">
+      	<div className="input-field col s6">
+  	 		<a className='dropdown-button btn' href='#' data-activates='gender'>Gender</a>
+
+  		<ul id='gender' className='dropdown-content'>
+    		<li><a href="#!">Male</a></li>
+    		<li><a href="#!">Female</a></li>
+    		<li><a href="#!">Prefer not to answer</a></li>
+  		</ul>
+  		</div>
+  	<div>
+  	 <div className="row">
+  	 	<div className="input-field col s12">
+  	 		<p> 3. Address Info </p> <hr/>
+  	 	</div>
+  	 </div>
+  	 	<div className="input-field col s10">
+          <input id="address" type="text" className="validate"></input>
+          <label htmlFor="address">Address</label>
+        </div>
+        <div className="input-field col s10">
+          <input id="address2" type="text" className="validate"></input>
+          <label htmlFor="address2">Address 2</label>
+        </div>
+     </div>
+
+     <div className="input-field col s12">
+  	 <a className='dropdown-button btn' href='#' data-activates='country'>Country</a>
+
+  		<ul id='country' className='dropdown-content'>
+    		<li><a href="#!">Position 1</a></li>
+    		<li><a href="#!">Position 2</a></li>
+    		<li><a href="#!">Position 3</a></li>
+    		<li><a href="#!">Position 4</a></li>
+    		<li><a href="#!">Position 5</a></li>
+  		</ul>
+
+  	</div> 
+  	<div className="input-field col s4">
+         <input id="city" type="text" className="validate"></input>
+         <label htmlFor="city">City</label>
+     </div>
+    <div className="input-field col s2">
+  	 <a className='dropdown-button btn' href='#' data-activates='state'>State</a>
+
+  		<ul id='state' className='dropdown-content'>
+    		<li><a href="#!">Position 1</a></li>
+    		<li><a href="#!">Position 2</a></li>
+    		<li><a href="#!">Position 3</a></li>
+    		<li><a href="#!">Position 4</a></li>
+    		<li><a href="#!">Position 5</a></li>
+  		</ul>
+
+  	</div> 
+  	<div className="input-field col s4">
+         <input id="zip" type="text" className="validate"></input>
+         <label htmlFor="zip">City</label>
+     </div>
+  	<div className="input-field col s12" style={{textAlign: 'center'}}>
+        <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+     </div>
+
+
+
+  	</div>
+
+     </div>
     </form>
   </div>
      </div>
