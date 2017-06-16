6c99e31c4eeef83da380b259b3dba8991418d53c
michiang <michiang@ucsd.edu>
2017-06-15 19:32:40 -0700

    Fix indentation for render html

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -18,6 +18,7 @@ export default class App extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
 
 	}  
+
 	componentDidMount() {
 	 
 	}
@@ -42,39 +43,40 @@ export default class App extends Component {
     event.preventDefault();
   }
 
-  render() {
+    render() {
     return (
-    <div>
-     <div className="card-panel purple simple-forms " style={{color: 'white'}}>Powered by SimpleForms</div>
-     <div style={{textAlign: 'center'}}>
-        <h1>Apply for SimpleForms</h1>
-        <p> Fill out the following form to apply for an open position at SimpleForms</p>
-     </div>
-    <div className="row">
-    <form className="col s12" onSubmit={this.handleSubmit}>
-        <p> 1. Selection Location/Position </p>
-        <hr/>
+      <div>
+        <div className="card-panel purple simple-forms " style={{color: 'white'}}>Powered by SimpleForms</div>
+          <div style={{textAlign: 'center'}}>
+            <h1>Apply for SimpleForms</h1>
+            <p> Fill out the following form to apply for an open position at SimpleForms</p>
+          </div>
+
+      <div className="row">
+        <form className="col s12" onSubmit={this.handleSubmit}>
+          <p> 1. Selection Location/Position </p>
+          <hr/>
     
-  		<div className="input-field col s6">
-      <select>
-        <option value="" disabled selected>Select Location</option>
-        <option value="1">San Diego</option>
-        <option value="2">Los Angeles</option>
-        <option value="3">San Francisco</option>
-      </select>
-    </div>
+      <div className="input-field col s6">
+        <select>
+          <option value="" disabled selected>Select Location</option>
+          <option value="1">San Diego</option>
+          <option value="2">Los Angeles</option>
+          <option value="3">San Francisco</option>
+        </select>
+      </div>
 
-  	 <div className="input-field col s6">
-      <select>
-        <option value="" disabled selected>Select Position</option>
-        <option value="1">Software Engineer</option>
-        <option value="2">Accountant</option>
-        <option value="3">Product Manager</option>
-      </select>
-    </div>
+      <div className="input-field col s6">
+        <select>
+          <option value="" disabled selected>Select Position</option>
+          <option value="1">Software Engineer</option>
+          <option value="2">Accountant</option>
+          <option value="3">Product Manager</option>
+        </select>
+      </div>
 
-  	<br/> <br/> <br/> <br />
-  	    <p> 2. Personal Info </p> <hr /> <br />
+      <br/> <br/> <br/> <br />
+      <p> 2. Personal Info </p> <hr /> <br />
       <div className="row">
         <div className="input-field col s6">
           <i className="material-icons prefix">account_circle</i>
@@ -102,35 +104,35 @@ export default class App extends Component {
         </div>
       </div>
       <div className="row">
-      	<div className="input-field col s7">
+        <div className="input-field col s7">
           <i className="material-icons prefix">phone</i>
           <input id="icon_telephone" type="tel" className="validate" required></input>
           <label htmlFor="icon_telephone" data-error="Invalid Phone Number" data-success="Valid Phone Number" className="active">Phone Number</label>
         </div>
       </div>
       <div className="row">
-      	<div className="input-field col s6">
+        <div className="input-field col s6">
           <i className="material-icons prefix">person_pin</i>
           <input type="date" className="datepicker"></input>
         </div>
       </div>
       
       <div className="row">
-      	<div className="input-field col s6">
-        <select>
+        <div className="input-field col s6">
+          <select>
             <option value="" disabled selected>Gender</option>
             <option value="1">Male</option>
             <option value="2">Female</option>
             <option value="3">Prefer not to answer</option>
           </select>
         </div>
-  	</div>
-  	 <div className="row">
-  	 	<div className="input-field col s12">
-  	 		<p> 3. Address Info </p> <hr/>
-  	 	</div>
-  	 </div>
-  	 	<div className="input-field col s10">
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <p> 3. Address Info </p> <hr/>
+        </div>
+      </div>
+        <div className="input-field col s10">
           <input id="address" type="text" className="validate" required></input>
           <label htmlFor="address">Address</label>
         </div>
@@ -139,40 +141,39 @@ export default class App extends Component {
           <label htmlFor="address2">Address 2</label>
         </div>
 
-     <div className="input-field col s12">
+      <div className="input-field col s12">
 
-  		<select>
-        <option value="" disabled selected>Country</option>
-        <option value="1">United States of America</option>
-        <option value="2">Canada</option>
-        <option value="3">United Kingdom</option>
-      </select>
+        <select>
+          <option value="" disabled selected>Country</option>
+          <option value="1">United States of America</option>
+          <option value="2">Canada</option>
+          <option value="3">United Kingdom</option>
+        </select>
 
-  	</div> 
-  	<div className="input-field col s4">
-         <input id="city" type="text" className="validate"></input>
-         <label htmlFor="city">City</label>
-     </div>
-    <div className="input-field col s2">
-  	 <select> 
-        <option value="" disabled selected>State</option>
-        <option value="1">California</option>
-        <option value="2">Colorado</option>
-        <option value="3">Washington</option>
-      </select>
+      </div> 
+        <div className="input-field col s4">
+          <input id="city" type="text" className="validate"></input>
+          <label htmlFor="city">City</label>
+        </div>
+      <div className="input-field col s2">
+        <select> 
+          <option value="" disabled selected>State</option>
+          <option value="1">California</option>
+          <option value="2">Colorado</option>
+          <option value="3">Washington</option>
+        </select>
 
-  	</div> 
-  	<div className="input-field col s4">
-         <input id="zip" type="text" className="validate" required></input>
-         <label htmlFor="zip">Zip Code</label>
-     </div>
-  	<div className="input-field col s12" style={{textAlign: 'center'}}>
-        <button className="btn waves-effect waves-light teal simple-forms" type="submit" name="action">Submit</button>
-     </div>
-
-    </form>
+      </div> 
+        <div className="input-field col s4">
+          <input id="zip" type="text" className="validate" required></input>
+          <label htmlFor="zip">Zip Code</label>
+        </div>
+        <div className="input-field col s12" style={{textAlign: 'center'}}>
+          <button className="btn waves-effect waves-light teal simple-forms" type="submit" name="action">Submit</button>
+        </div>
+      </form>
+    </div>
   </div>
-     </div>
-    );
-  }
+  );
+ }
 }
